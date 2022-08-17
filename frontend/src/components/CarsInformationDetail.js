4a5c77b00b651e4f97e6b3c06d58ce54641f7d21
sagharnoursobhi <saghar.noursobhi@hyperisland.se>
2022-08-17 12:59:11 +0200

    Creating frontend folder

--- frontend/src/components/CarsInformationDetail.js
+++ frontend/src/components/CarsInformationDetail.js
@@ -6,15 +6,15 @@ import LocalTaxiIcon from '@material-ui/icons/LocalTaxi';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
+import Navbar from './Navbar';
+import Footer from './Footer';
 import { useState, useEffect } from 'react';
 import ErrorModal from "./ErrorModal";
+import emailjs from 'emailjs-com';
 
 
 export default function CarsInformationDetail() {
-    const { id } = useParams();
-    let result = data.find(el => el.id === id);
+    
     const [fullName, setFullName] = useState('');
     const [email, setEmail] = useState('');
     const [phoneNum, setPhoneNum] = useState('');
@@ -22,6 +22,12 @@ export default function CarsInformationDetail() {
     const [error, setError] = useState();
     const [rentDuration, setRentDuration] = useState('');
     const [radioError, setRadioError] = useState(false);
+
+    const { id } = useParams();
+    let result = data.find((el) => el.id === id);
+
+    const [carInfo, setCarInfo] = useState(result.carName);
+    
 
     const onConfirm = () => {
         setError(null);
@@ -44,13 +50,18 @@ export default function CarsInformationDetail() {
         }
 
         setNewUser([...newUser, {name: fullName, email: email, phone: phoneNum, duration: rentDuration}]);
-        setFullName('');
-        setEmail('');
-        setPhoneNum('');
-        setRentDuration('');
-    }
+        emailjs.sendForm('service_2kqicfw', 'template_xcpb7xo', e.target,'IH1EU_T7Ah9PeLeEz').then(response => {
+            console.log('SUCCESS!', response);
 
-    console.log(newUser)
+            setFullName('');
+            setEmail('');
+            setPhoneNum('');
+            setRentDuration('');
+
+          }, error => {
+            console.log('FAILED...', error);
+          });
+      }
 
     useEffect(() => {
         setTimeout(() => {
@@ -68,34 +79,38 @@ export default function CarsInformationDetail() {
                     <div>{result.rentalPrices}</div>
                 </div>
                 <Form className="mt-3 mb-3 rent-form" onSubmit={onSubmithandler}>
+                    <div className="mb-4 d-flex justify-content-start align-items-start">
+                        <Form.Check name="carInfo" checked={ carInfo === result.carName } type="radio" onChange={(e) => setRentDuration(e.target.value)} value={carInfo}/>
+                        <Form.Label className="mb-0 form-title">{ carInfo }</Form.Label>
+                    </div>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Full Name</Form.Label>
-                        <Form.Control onChange={(e)=>setFullName(e.target.value)} value={fullName} type="text" placeholder="Enter your name" />
+                        <Form.Control name="fullName" onChange={(e)=>setFullName(e.target.value)} value={fullName} type="text" placeholder="Enter your name" />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control onChange={(e)=>setEmail(e.target.value)} value={email} type="email" placeholder="Enter email" />
+                        <Form.Control name="email" onChange={(e)=>setEmail(e.target.value)} value={email} type="email" placeholder="Enter email" />
                         <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                         </Form.Text>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Phone</Form.Label>
-                        <Form.Control onChange={(e)=>setPhoneNum(e.target.value)} value={phoneNum} type="text" placeholder="Telephone" />
+                        <Form.Control name="phone" onChange={(e)=>setPhoneNum(e.target.value)} value={phoneNum} type="text" placeholder="Telephone" />
                     </Form.Group>
                     <Form.Group className="mb-3 d-flex flex-column justify-content-start align-items-start">
                         <label htmlFor="">How long would you like to rent this car?</label>
                         {radioError && <span className="text-danger mb-2">This field is required!</span>}
                         <div className="d-flex justify-content-center align-items-center">
-                            <Form.Check className="ml-3" checked={rentDuration === "3 days"} type="radio" onChange={(e) => setRentDuration(e.target.value)} value="3 days"/>
+                            <Form.Check className="ml-3" name="rentDuration" checked={rentDuration === "3 days"} type="radio" onChange={(e) => setRentDuration(e.target.value)} value="3 days"/>
                             <Form.Label className="mb-0">3 days</Form.Label>
                         </div>
                         <div className="d-flex justify-content-center align-items-center mt-3">
-                            <Form.Check className="ml-3" type="radio" checked={rentDuration === "6 days"} onChange={(e) => setRentDuration(e.target.value)} value="6 days"/>
+                            <Form.Check name="rentDuration" className="ml-3" type="radio" checked={rentDuration === "6 days"} onChange={(e) => setRentDuration(e.target.value)} value="6 days"/>
                             <Form.Label className="mb-0">6 days</Form.Label>
                         </div>
                         <div className="d-flex justify-content-center align-items-center mt-3">
-                            <Form.Check className="ml-3" type="radio" checked={rentDuration === "10 days"} onChange={(e) => setRentDuration(e.target.value)} value="10 days"/>
+                            <Form.Check name="rentDuration" className="ml-3" type="radio" checked={rentDuration === "10 days"} onChange={(e) => setRentDuration(e.target.value)} value="10 days"/>
                             <Form.Label className="mb-0">10 days</Form.Label>
                         </div>
                     </Form.Group>
